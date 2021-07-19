b60ea3200304f16d6e9d6d227fe5c1a08842ab1c
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2021-07-19 16:43:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ showarrow=False, xanchor='left', text='Candlestick Chart')]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div([
 dcc.Graph(
